0da831cd4e9a40b9e9adc7d2b6bde4a12684a672
Iwo Plaza <iwoplaza@gmail.com>
2026-05-11 15:51:55 +0200

    test: Regression tests for explicit disables

---
+++ packages/tsover/tests/fixtures/disable-inactive/expr.ts
@@ -0,0 +1,5 @@
+'use tsover';
+
+import { Vec2f } from '../vec';
+
+export const result = new Vec2f(1, 2) + new Vec2f(3, 4);
---
+++ packages/tsover/tests/fixtures/no-directive/expr.ts
@@ -0,0 +1,3 @@
+import { Vec2f } from '../vec';
+
+export const result = new Vec2f(1, 2) + new Vec2f(3, 4);
